22740fff413835fc0fd34b65ae4ad779eb69ee49
css <10941629+Caspertjuh@users.noreply.github.com>
2018-11-01 12:55:58 +0100

    Updated to working, added support for specific array of streamers

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from requests import get
 import os
 
 url = 'https://overrustlelogs.net/'
+streamers = ['Admiralbulldog', 'Alinity', 'Alkaizer', 'Amouranth', 'Andymilonakis', 'Asmongold', 'Athenelive', 'B0aty',
+             'Becca', 'Bullyhunters', 'C9sneaky', 'Cdewx', 'Celeste', 'Cjayride', 'Clintstevenz', 'Cowsep', 'Dafran',
+             'Dankquan', 'Dansgaming', 'Darksydephil', 'Dekar173', 'Demolition_d', 'Dendi', 'Destiny', 'Destinygg',
+             'Eloise', 'Esfandtv', 'Forsen', 'Geersart', 'Geisha', 'Greekgodx', 'Grossie_gore', 'H3h3productions',
+             'Hyubsama', 'Iam_choa', 'Ice_poseidon', 'Jakenbakelive', 'Jaxerie', 'Kaceytron', 'Legendarylea',
+             'Loltyler1', 'Mcconnellret', 'Meteos', 'Mitchjones', 'Nani', 'Nmplol', 'Nymn', 'Pajlada', 'Reckful',
+             'Sick_nerd', 'Singsing', 'Sodapoppin', 'Trihardgodcx', 'Xqcow']
 
 response = get(url)
 channels = []
@@ -19,16 +26,18 @@ if not os.path.exists('./logs'):
 else:
     print('Log folder already exists, skipping creating one')
 
-channel_containers = html_soup.find_all('a', class_ = 'collection-item')
+channel_containers = html_soup.find_all('a', class_ = 'list-group-item')
 for channel in channel_containers:
-    channels.append(channel['href'])
+    for streamer in streamers:
+        if channel['href'].startswith(streamer, 1):
+            channels.append(channel['href'])
 for id,channel in enumerate(channels):
     months = []
-    url = 'https://overrustlelogs.net' + channel 
+    url = 'https://overrustlelogs.net' + channel
     response = get(url)
     html_soup = BeautifulSoup(response.text, 'html.parser')
     type(html_soup)
-    month_containers = html_soup.find_all('a', class_ = 'collection-item')
+    month_containers = html_soup.find_all('a', class_ = 'list-group-item')
     for month in month_containers:
         months.append(month['href'])
     for month in months:
@@ -46,7 +55,7 @@ for id,channel in enumerate(channels):
         response = get(url)
         html_soup = BeautifulSoup(response.text, 'html.parser')
         type(html_soup)
-        day_containers = html_soup.find_all('a', class_ = 'collection-item')
+        day_containers = html_soup.find_all('a', class_ = 'list-group-item')
         for day in day_containers:
             days.append(day['href'])
         for day in days:
